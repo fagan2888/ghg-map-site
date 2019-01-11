d5b550db294d854aaf0eb680570000bf53b57451
Rick Lupton <mail@ricklupton.name>
2019-01-11 11:56:23 +0000

    Update tour

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p public/data
-curl https://raw.githubusercontent.com/ricklupton/ghg-map-2014-data/v1.0/sankey_data.json > public/data/sankey_data.json
+curl https://raw.githubusercontent.com/ricklupton/ghg-map-2014-data/7b68cb2d23df2f50dade383af95f51179d80d552/sankey_data.json > public/data/sankey_data.json
--- src/intro.js
+++ src/intro.js
@@ -34,28 +34,71 @@ represents the amount of emissions that can be attributed to a given activity. `
     }, {
       element: findGroupElement('Service'),
       intro: `
-The diagram is structured in seven columns that are used to link human activities (on the left)
-to ...`
+The diagram is structured in seven columns that are used to link <b>human
+activities</b> (on the left) to ...`
     }, {
       element: findGroupElement('Emissions'),
       intro: `
-... to GHG emissions (on the right). You can click on any marked node to see
-mode details and learn more.
+... to <b>GHG emissions</b> (on the right). You can click on any marked node to
+see mode details and learn more.
 
-To better understand how to read the diagram, let
-us guide you through a short example that explores how commuting by car is
-linked to global emissions.
+<p>To better understand how to read the diagram, let us guide you through a
+short example that explores how commuting by car is linked to global emissions.
 
-***At this point have buttons to either (a) let me explore, or (b) show me an
-***example.
+<p><b>Click "next" to continue with the example,</b>, or click "skip" to explore
+yourself.`
+    }, {
+      intro: `
+We have all heard that commuting by car is detrimental for climate change,
+thanks to this diagram we can better understand and put into perspective its
+effect.`
+    }, {
+      element: findNodeElement('FinalService^Mobility'),
+      intro: `
+First of all, commuting is part of the larger service that is “mobility” so see
+specifically the emissions associated with commuting, we need to expand this
+node.
+
+<p><b>⟶ Click the "mobility" node to expand it</b>
 `
     }, {
-      element: findNodeElement('Sector^Transport'),
-      intro: 'Hello! This is the Sankey diagram.'
+      element: findNodeElement('FinalService^Mobility'),
+      intro: `
+Here we can see that commuting accounts for xx% of the emissions associated with
+mobility while most of the emissions are associated with the broader category of
+personal travel. You can see that most of the emissions associated with
+commuting flow into the transport sector category,
+`
+    }, {
+      element: '#details',
+      intro: `More details about the selected node are shown here.`
     }
   ]
 
-  intro.setOptions({steps: intro1Steps})
-  intro.start()
+  intro
+    .setOptions({steps: intro1Steps})
+    .onchange(onchange)
+    .start()
 }
 
+let mobilityFirstTimeDone = false
+let expandedMobility = false
+
+function onchange (elem) {
+  console.log('onchange', elem)
+  const d = elem.__data__ || {}
+
+  if (d.id === 'FinalService^Mobility') {
+    if (!mobilityFirstTimeDone) {
+      mobilityFirstTimeDone = true
+    } else {
+      if (d.ports.length === 1 && !expandedMobility) {
+        console.log('trying to expand')
+        const e = document.createEvent('UIEvents')
+        e.initUIEvent('click', true, true, /* ... */)
+        elem.dispatchEvent(e)
+      }
+      expandedMobility = true
+    }
+  }
+}
